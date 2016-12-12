9e5efccd9f79c3012aa8ae856d54dabf716f8ffa
0xZhangKe <zk721195@live.com>
2016-12-13 00:13:15 +0800

    添加沉浸式状态栏工具

--- app/src/main/java/com/zhangke/funnyread/utils/UiTools.java
+++ app/src/main/java/com/zhangke/funnyread/utils/UiTools.java
@@ -1,6 +1,10 @@
 package com.zhangke.funnyread.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
 import android.widget.Toast;
 
 /**
@@ -17,5 +21,21 @@ public class UiTools {
             mToast.setText(msg);
         }
         mToast.show();
+    }
+
+    /**
+     * 设置Window Title颜色<br/>
+     * @param activity activity
+     */
+    public static void setWindow(Activity activity){
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = activity.getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }else{
+            //TODO set level api window
+        }
     }
 }
