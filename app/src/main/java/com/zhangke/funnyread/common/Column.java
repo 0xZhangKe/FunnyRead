2426447daa7cefe277ec006b76f7becb81ed2b1a
0xZhangKe <zk721195@live.com>
2016-12-14 23:55:46 +0800

    添加常量类

---
+++ app/src/main/java/com/zhangke/funnyread/common/Column.java
@@ -0,0 +1,28 @@
+package com.zhangke.funnyread.common;
+
+/**
+ * Created by ZhangKe at 2016/12/14
+ */
+public class Column {
+
+    /**
+     * 数据库版本
+     */
+    public static final int DB_VERSION=1;
+    /**
+     * DB名字
+     */
+    public static final String DB_NAME ="History";
+    /**
+     * 知乎表名
+     */
+    public static final String DB_ZHIHU_TABLE_NAME ="ZHIHU_HISTORY";
+    /**
+     * 豆瓣表名
+     */
+    public static final String DB_DOUBAN_TABLE_NAME ="DOUBAN_HISTORY";
+    /**
+     * 果壳表名
+     */
+    public static final String DB_GUOKE_TABLE_NAME ="GUOKE_HISTORY";
+}
